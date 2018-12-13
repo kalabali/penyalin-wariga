5170d7907525bc11b1ecbc5889438227a22c1d97
imdbsd <budisuryadarma0407@gmail.com>
2018-12-14 06:31:01 +0800

    first init rewrite

--- src/__tests__/calendar-bali-info.test.js
+++ src/__tests__/calendar-bali-info.test.js
@@ -84,156 +84,165 @@ it("throw error when year is below 0", () => {
     );
 });
 
-it('Getting rerainan and memorial day on november 2018', () => {
+it("should return all information in december 2018", () => {
     expect.assertions(1);
     return calendarBaliInfo({
-        month: 11,
+        month: 12,
         year: 2018
     })
-        .then(data => {
-            expect(data).toEqual(
-                expect.objectContaining({
-                    "rerainanDay": expect.arrayContaining([
-                        {
-                            "date": "2",
-                            "month": "Nopember",
-                            "year": 2018,
-                            "events": [
-                                {
-                                    "type": "rerainan",
-                                    "event_name": "Hari Bhatari Sri",
-                                    "information": ""
-                                }
-                            ]
-                        },
-                        {
-                            "date": "6",
-                            "month": "Nopember",
-                            "year": 2018,
-                            "events": [
-                                {
-                                    "type": "rerainan",
-                                    "event_name": "Kajeng Kliwon Uwudan",
-                                    "information": ""
-                                },
-                                {
-                                    "type": "rerainan",
-                                    "event_name": "Hari Anggarakasih Kulantir",
-                                    "information": ""
-                                }
-                            ]
-                        },
-                        {
-                            "date": "7",
-                            "month": "Nopember",
-                            "year": 2018,
-                            "events": [
-                                {
-                                    "type": "rerainan",
-                                    "event_name": "Hari Tilem Sasih Kalima",
-                                    "information": ""
-                                }
-                            ]
-                        },
-                        {
-                            "date": "21",
-                            "month": "Nopember",
-                            "year": 2018,
-                            "events": [
-                                {
-                                    "type": "rerainan",
-                                    "event_name": "Hari Buda Kliwon Gumbreg",
-                                    "information": ""
-                                },
-                                {
-                                    "type": "rerainan",
-                                    "event_name": "Kajeng Kliwon Enyitan",
-                                    "information": ""
-                                }
-                            ]
-                        },
-                        {
-                            "date": "22",
-                            "month": "Nopember",
-                            "year": 2018,
-                            "events": [
-                                {
-                                    "type": "rerainan",
-                                    "event_name": "Hari Purnama Kanem",
-                                    "information": ""
-                                }
-                            ]
-                        }
-                    ])
-                }),
-                expect.objectContaining({
-                    "memorialDay": expect.arrayContaining([
-                        {
-                            "date": "10",
-                            "month": "Nopember",
-                            "year": 2018,
-                            "events": [
-                                {
-                                    "type": "hari-peringatan",
-                                    "event_name": "Hari Pahlawan",
-                                    "information": ""
-                                }
-                            ]
-                        },
-                        {
-                            "date": "15",
-                            "month": "Nopember",
-                            "year": 2018,
-                            "events": [
-                                {
-                                    "type": "hari-peringatan",
-                                    "event_name": "Hari Hening dan Doa Nasional",
-                                    "information": ""
-                                }
-                            ]
-                        },
-                        {
-                            "date": "20",
-                            "month": "Nopember",
-                            "year": 2018,
-                            "events": [
-                                {
-                                    "type": "hari-peringatan",
-                                    "event_name": "Hari Puputan Margarana (regional)",
-                                    "information": ""
-                                }
-                            ]
-                        },
-                        {
-                            "date": "25",
-                            "month": "Nopember",
-                            "year": 2018,
-                            "events": [
-                                {
-                                    "type": "hari-peringatan",
-                                    "event_name": "Hari PGRI",
-                                    "information": ""
-                                }
-                            ]
-                        },
-                        {
-                            "date": "29",
-                            "month": "Nopember",
-                            "year": 2018,
-                            "events": [
-                                {
-                                    "type": "hari-peringatan",
-                                    "event_name": "Hari Korpri",
-                                    "information": ""
-                                }
-                            ]
-                        }
-                    ])
-                })
-            )
+    .then(data => {
+        expect(data).toEqual({
+            "wuku": ['Wariga', 'Warigadean', 'Julungwangi', 'Sungsang', 'Dungulan', 'Kuningan']
         })
+    })
 })
 
-// expect.arrayContaining([
-
-// ])
+// it('Getting rerainan and memorial day on november 2018', () => {
+//     expect.assertions(1);
+//     return calendarBaliInfo({
+//         month: 11,
+//         year: 2018
+//     })
+//         .then(data => {
+//             expect(data).toEqual(
+//                 expect.objectContaining({
+//                     "rerainanDay": expect.arrayContaining([
+//                         {
+//                             "date": "2",
+//                             "month": "Nopember",
+//                             "year": 2018,
+//                             "events": [
+//                                 {
+//                                     "type": "rerainan",
+//                                     "event_name": "Hari Bhatari Sri",
+//                                     "information": ""
+//                                 }
+//                             ]
+//                         },
+//                         {
+//                             "date": "6",
+//                             "month": "Nopember",
+//                             "year": 2018,
+//                             "events": [
+//                                 {
+//                                     "type": "rerainan",
+//                                     "event_name": "Kajeng Kliwon Uwudan",
+//                                     "information": ""
+//                                 },
+//                                 {
+//                                     "type": "rerainan",
+//                                     "event_name": "Hari Anggarakasih Kulantir",
+//                                     "information": ""
+//                                 }
+//                             ]
+//                         },
+//                         {
+//                             "date": "7",
+//                             "month": "Nopember",
+//                             "year": 2018,
+//                             "events": [
+//                                 {
+//                                     "type": "rerainan",
+//                                     "event_name": "Hari Tilem Sasih Kalima",
+//                                     "information": ""
+//                                 }
+//                             ]
+//                         },
+//                         {
+//                             "date": "21",
+//                             "month": "Nopember",
+//                             "year": 2018,
+//                             "events": [
+//                                 {
+//                                     "type": "rerainan",
+//                                     "event_name": "Hari Buda Kliwon Gumbreg",
+//                                     "information": ""
+//                                 },
+//                                 {
+//                                     "type": "rerainan",
+//                                     "event_name": "Kajeng Kliwon Enyitan",
+//                                     "information": ""
+//                                 }
+//                             ]
+//                         },
+//                         {
+//                             "date": "22",
+//                             "month": "Nopember",
+//                             "year": 2018,
+//                             "events": [
+//                                 {
+//                                     "type": "rerainan",
+//                                     "event_name": "Hari Purnama Kanem",
+//                                     "information": ""
+//                                 }
+//                             ]
+//                         }
+//                     ])
+//                 }),
+//                 expect.objectContaining({
+//                     "memorialDay": expect.arrayContaining([
+//                         {
+//                             "date": "10",
+//                             "month": "Nopember",
+//                             "year": 2018,
+//                             "events": [
+//                                 {
+//                                     "type": "hari-peringatan",
+//                                     "event_name": "Hari Pahlawan",
+//                                     "information": ""
+//                                 }
+//                             ]
+//                         },
+//                         {
+//                             "date": "15",
+//                             "month": "Nopember",
+//                             "year": 2018,
+//                             "events": [
+//                                 {
+//                                     "type": "hari-peringatan",
+//                                     "event_name": "Hari Hening dan Doa Nasional",
+//                                     "information": ""
+//                                 }
+//                             ]
+//                         },
+//                         {
+//                             "date": "20",
+//                             "month": "Nopember",
+//                             "year": 2018,
+//                             "events": [
+//                                 {
+//                                     "type": "hari-peringatan",
+//                                     "event_name": "Hari Puputan Margarana (regional)",
+//                                     "information": ""
+//                                 }
+//                             ]
+//                         },
+//                         {
+//                             "date": "25",
+//                             "month": "Nopember",
+//                             "year": 2018,
+//                             "events": [
+//                                 {
+//                                     "type": "hari-peringatan",
+//                                     "event_name": "Hari PGRI",
+//                                     "information": ""
+//                                 }
+//                             ]
+//                         },
+//                         {
+//                             "date": "29",
+//                             "month": "Nopember",
+//                             "year": 2018,
+//                             "events": [
+//                                 {
+//                                     "type": "hari-peringatan",
+//                                     "event_name": "Hari Korpri",
+//                                     "information": ""
+//                                 }
+//                             ]
+//                         }
+//                     ])
+//                 })
+//             )
+//         })
+// })
--- src/modules/calendar-bali-info.js
+++ src/modules/calendar-bali-info.js
@@ -3,7 +3,7 @@ const htmlFetch = require('../helpers/html-fetch');
 const utils = require('../helpers/utils');
 const { monthChecker, checkObject, yearChecker } = require('../helpers/validators');
 
-const calendarBaliInfo = async (options) => {    
+const calendarBaliInfo = async options => {    
     try {
         checkObject(options, ['month', 'year']);
         const { month, year } = options;
@@ -14,65 +14,17 @@ const calendarBaliInfo = async (options) => {
         const html = await htmlFetch(`http://kalenderbali.info/?month=${month}&year=${year}`);        
         const $ = cheerio.load(html);
 
-        let rerainanCol = $("#right-column-1 .box table tbody").html();
-        let memorialCol = $("#right-column-2 .box table tbody").html().replace(/<\/tr><tr>/g, "<tr>||</tr>").split("||");
-
-        if (!rerainanCol || !memorialCol) {
-            throw new Error('There is an error in resource.');
-        }
-        rerainanCol = rerainanCol.replace(/<\/tr><tr>/g, "<tr>||</tr>").split("||");
-        let rerainanDay = [];
-        rerainanCol.forEach(r => {
-            const d = $(r).text().split(".");
-            let storedR = {
-                date: d[0],
-                month: utils.getFullMonth(month),
-                year: year,
-                events: []
-            }
-            d[1].split(";").forEach(e => {
-                let event = e.split(",");
-                storedR.events.push({
-                    type: "rerainan",
-                    event_name: event[0].trim(),
-                    information: (event[1] !== undefined) ? event[1].trim() : ""
-                })
-            })
-            rerainanDay.push(storedR);
-        });
-
-        let memorialDay = [];
-        memorialCol.forEach(r => {
-            const d = $(r).text().split(".");
-            let storedP = {
-                date: d[0],
-                month: utils.getFullMonth(month),
-                year: year,
-                events: []
-            }
-            d[1].split(";").forEach(e => {
-                let event = e.split(",");
-                storedP.events.push({
-                    type: "hari-peringatan",
-                    event_name: event[0].trim(),
-                    information: (event[1] !== undefined) ? event[1].trim() : ""
-                })
-            })
-            memorialDay.push(storedP);
-        });
-
-        return {
-            rerainanDay,
-            memorialDay
-        };
+        const wuku = $('.judulAtas.orange').map((index, el) => $(el).text().trim()).get()
+        console.log({wuku})
+        return { wuku: wuku };
     }
     catch (err) {
         throw new Error(err);
     }
 }
 
-// calendarBaliInfo({year: 2018, month: 1 })
-//     .then(result => {  console.log(JSON.stringify(result)); })
+calendarBaliInfo({year: 2018, month: 12 })
+    .then(result => {  console.log(JSON.stringify(result)); })
 
 module.exports = calendarBaliInfo;
 
